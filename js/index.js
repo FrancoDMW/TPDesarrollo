c53d29ff5668259771f9751d5c510c2c0c233620
FrancoDMW <franco.ulzurrun.dmw@gmail.com>
2024-06-17 22:15:27 -0300

    js index

---
+++ js/index.js
@@ -0,0 +1,33 @@
+window.onload = function () {
+    var enviar = document.getElementById("enviar");
+    enviar.addEventListener("click", function (event) {
+        event.preventDefault(); // Evitar el comportamiento predeterminado del formulario por el submit
+        EnviarForm();
+    });
+}
+
+function EnviarForm() {
+    var validar = true;
+    var formulario = document.getElementById("formulario");
+    var usuario = document.getElementById("textUsuario");
+    let contraseña = document.getElementById("textContraseña");
+    if (!usuario.value) {
+        alert("Por favor, ingrese su usuario");
+        usuario.focus();
+        validar = false;
+    }
+    if (!contraseña.value) {
+        alert("Por favor, ingrese su contraseña");
+        contraseña.focus();
+        validar = false;
+    }
+    if (validar) {
+        let user = usuario.value;
+        let pass = contraseña.value;
+        localStorage.setItem("usuario", user);
+        localStorage.setItem("contraseña", pass);
+        formulario.submit();
+        alert("Validación exitosa");
+        window.location.href = "./pages/home.html";
+    }
+}
